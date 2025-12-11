3c97a4eb8e8d6e278c2510b1d0d5e42e9c318e10
rawan95rsh-blip <rawan95.rsh@gmail.com>
2025-12-11 08:44:00 +0300

    done filtering file

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,40 +1,15 @@
+
 /**
- * Use this array of numbers if you want to
- * manually test your functions
+ * Use this array of numbers if you want to manually test your functions
  */
 const numbers = [10, 13, 20, 25, 38, 35, 40];
 
-// `squaredNumbers` function that:
-// - Accepts "numbers" parameter of type "number[]".
-// - Return a new array that contains each number squared.
-// example:
-// squaredNumbers(numbers); // => [100, 169, 400, 625, 1444, 1225, 1600]
-function squaredNumbers(numbers: number[]): number[] {
-  // write your code here...
-  const numbers = [10, 13, 20, 25, 38, 35, 40];
-
-  // 3) Square each number
-  export function squareNumbers(): number[] {
-    return numbers.map((num) => num * num);
-  }
-  
-  // 4) Multiply each number by 2
-  export function doubleNumbers(): number[] {
-    return numbers.map((num) => num * 2);
-  }
-  
-  return []; // replace empty array with what you see is fit
+// 3) Create a new array that contains each number squared.
+export function squaredNumbers(nums: number[] = numbers): number[] {
+  return nums.map((num) => num * num);
 }
 
-// `doubledNumbers` function that:
-// - Accepts "numbers" parameter of type "number[]".
-// - Return a new array that contains each number multiplied by 2.
-// example:
-// doubledNumbers(numbers); // => [20, 26, 40, 50, 76, 70, 80]
-function doubledNumbers(numbers: number[]): number[] {
-  // write your code here...
-
-  return []; // replace empty array with what you see is fit
+// 4) Create a new array that contains each number multiplied by 2.
+export function doubledNumbers(nums: number[] = numbers): number[] {
+  return nums.map((num) => num * 2);
 }
-
-export { squaredNumbers, doubledNumbers };
